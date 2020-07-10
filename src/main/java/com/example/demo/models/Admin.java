ef853e9e43b4d411defa1fdeacae1c92793d7e74
Martinsglp <67226518+Martinsglp@users.noreply.github.com>
2020-07-10 14:56:40 +0300

    Updated
    All models working, can see table

--- src/main/java/com/example/demo/models/Admin.java
+++ src/main/java/com/example/demo/models/Admin.java
@@ -1,11 +1,21 @@
 package com.example.demo.models;
 
+import java.io.Serializable;
+import java.util.Collection;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import com.example.demo.models.enums.userType;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,16 +23,35 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Entity
-@Table(name = "Admin")
+@Table(name = "AdminTable")
 @Getter @Setter @ToString @NoArgsConstructor
-public class Admin extends User{
+public class Admin extends User implements Serializable{
 
 	@Id
 	@Column(name = "AD_ID")
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int ad_ID;
 	
+	@Column(name = "Type")
+	private userType type;
+
+	public Admin(@Size(min = 4, max = 30) @Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!") String username,
+			@Size(min = 6, max = 30) @Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!") String password,
+			@Size(min = 4, max = 30) @Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!") String name,
+			@Size(min = 4, max = 30) @Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!") String surname,
+			@Size(min = 4, max = 30) @Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!") String email,
+			userType type) {
+		super(username, password, name, surname, email);
+		this.type = type;
+	}
+
+
 	
-	private int role_ID;
+	
+	
+	/*
+	@OneToOne(mappedBy = "user")
+	private User users;
+	*/
 	
 }
--- src/main/java/com/example/demo/models/VIPUser.java
+++ src/main/java/com/example/demo/models/VIPUser.java
@@ -6,6 +6,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import com.example.demo.models.enums.userType;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,7 +30,20 @@ public class VIPUser extends User{
 	private boolean vip_priority;
 	
 	
-	private int role_ID;
+	@Column(name = "Type")
+	private userType type;
+
+
+	public VIPUser(@Size(min = 4, max = 30) @Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!") String username,
+			@Size(min = 6, max = 30) @Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!") String password,
+			@Size(min = 4, max = 30) @Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!") String name,
+			@Size(min = 4, max = 30) @Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!") String surname,
+			@Size(min = 4, max = 30) @Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!") String email,
+			boolean vip_priority, userType type) {
+		super(username, password, name, surname, email);
+		this.vip_priority = vip_priority;
+		this.type = type;
+	}
 	
 	
 	
