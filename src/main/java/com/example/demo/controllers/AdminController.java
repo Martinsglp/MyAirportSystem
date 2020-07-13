cb63963c1a93bc2b0c03858e7b62b4a45c39d310
Martinsglp <67226518+Martinsglp@users.noreply.github.com>
2020-07-13 13:17:50 +0300

    minifix
    renamed package services

--- src/main/java/com/example/demo/controllers/AdminController.java
+++ src/main/java/com/example/demo/controllers/AdminController.java
@@ -1,7 +1,7 @@
 package com.example.demo.controllers;
 
 import com.example.demo.models.Flight;
-import com.example.demo.services.IFlightService;
+import com.example.demo.service.IFlightService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
