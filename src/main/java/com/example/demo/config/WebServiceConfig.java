9a6554d6f490d247fbbc24768e37627062d3a4f8
Marexglp <kurpnieks.mareks@gmail.com>
2020-07-13 16:07:16 +0300

    Security

--- src/main/java/com/example/demo/config/WebServiceConfig.java
+++ src/main/java/com/example/demo/config/WebServiceConfig.java
@@ -1,5 +1,56 @@
 package com.example.demo.config;
 
-public class WebServiceConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+public class WebServiceConfig extends WebSecurityConfigurerAdapter{
+
+	 @Override
+        protected UserDetailsService userDetailsService() {
+                InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+                
+                manager.createUser(User.withDefaultPasswordEncoder().username("admin").password("123").roles("ADMIN").build());
+                manager.createUser(User.withDefaultPasswordEncoder().username("user").password("123").roles("USER").build());
+                return manager;
+        }
+	
+	 
+	//Encryption for passwords
+	@Bean
+        public PasswordEncoder passwordEncoder()
+        {
+                return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        }
+	
+	
+	
+//	@Override
+//      protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//              auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+//      }
+	
+	
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+            .antMatchers("/h2-console/**").permitAll()
+            .anyRequest().authenticated();
+
+            
+          
+            //http.csrf().disable();
+            //http.headers().frameOptions().disable();
+                    
+    }
+	
+	
+	
 
 }
