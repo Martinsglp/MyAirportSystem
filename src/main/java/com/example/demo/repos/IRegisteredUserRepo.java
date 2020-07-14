27a417f8ff01804ecd9545462d713fe51d7d190b
Martinsglp <67226518+Martinsglp@users.noreply.github.com>
2020-07-14 11:14:27 +0300

    Update
    updated Register user with functions

--- src/main/java/com/example/demo/repos/IRegisteredUserRepo.java
+++ src/main/java/com/example/demo/repos/IRegisteredUserRepo.java
@@ -1,9 +1,18 @@
 package com.example.demo.repos;
 
+import java.util.ArrayList;
+
 import org.springframework.data.repository.CrudRepository;
 
+import com.example.demo.models.BoardingPass;
 import com.example.demo.models.RegisteredUser;
 
 public interface IRegisteredUserRepo extends CrudRepository<RegisteredUser, Integer>{
-
+	
+	boolean existsByNameAndSurnameAndPassword(String name, String surname, String password);
+	
+	boolean existsByUsernameAndPassword(String username, String password);
+	
+	
+	
 }
