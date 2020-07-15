d71a0c22f6deb68df7399e83db3cbba9c1540108
Syact <katrinabluma@gmail.com>
2020-07-15 11:00:12 +0300

    fixed validations

--- src/main/java/com/example/demo/models/RegisteredUser.java
+++ src/main/java/com/example/demo/models/RegisteredUser.java
@@ -7,10 +7,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
@@ -32,7 +32,8 @@ public class RegisteredUser extends User{
 	private int rus_ID;
 	
 	@Column(name = "Extra_Points")
-	@Size(min = 0, max = 100)
+	@Min(0)
+	@Max(100)
 	private int Extra_points;
 	
 	/*@Id
@@ -43,13 +44,17 @@ public class RegisteredUser extends User{
 	@Column(name = "Type")
 	private userType type;
 
-	public RegisteredUser(
-			@Size(min = 4, max = 30) @Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!") String username,
-			@Size(min = 6, max = 30) @Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!") String password,
-			@Size(min = 4, max = 30) @Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!") String name,
-			@Size(min = 4, max = 30) @Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!") String surname,
-			@Size(min = 4, max = 30) @Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!") String email,
-			@Size(min = 0, max = 100) int extra_points, userType type) {
+	public RegisteredUser(@Size(min = 0, max = 100) int extra_points, userType type) {
+		Extra_points = extra_points;
+		this.type = type;
+	}
+
+	public RegisteredUser(@Size(min = 4, max = 30) @Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!") String username,
+						  @Size(min = 3, max = 30) String password,
+						  @Size(min = 3, max = 30) @Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!") String name,
+						  @Size(min = 3, max = 30) @Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!") String surname,
+						  @Size(min = 4, max = 30) @Pattern(regexp = "[A-Za-z0-9+_.-]+@(.+)$", message = "Check name!") String email,
+						  int extra_points, userType type) {
 		super(username, password, name, surname, email);
 		Extra_points = extra_points;
 		this.type = type;
--- src/main/java/com/example/demo/models/User.java
+++ src/main/java/com/example/demo/models/User.java
@@ -1,19 +1,7 @@
 package com.example.demo.models;
 
-import java.util.Collection;
-
 import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.persistence.MappedSuperclass;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
@@ -32,17 +20,17 @@ public class User {
 	private String username;
 	
 	@Column(name="Password")
-	@Size(min=6, max=30)
-	@Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!")
+	@Size(min=3, max=30)
+//	@Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!")
 	private String password;
 	
 	@Column(name="Name")
-	@Size(min=4, max=30)
+	@Size(min=3, max=30)
 	@Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!")
 	private String name;
 	
 	@Column(name="SurName")
-	@Size(min=4, max=30)
+	@Size(min=3, max=30)
 	@Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!")
 	private String surname;
 	
@@ -53,20 +41,14 @@ public class User {
 	private String email;
 
 	public User(@Size(min = 4, max = 30) @Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!") String username,
-			@Size(min = 6, max = 30) @Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!") String password,
-			@Size(min = 4, max = 30) @Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!") String name,
-			@Size(min = 4, max = 30) @Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!") String surname,
-			@Size(min = 4, max = 30) @Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!") String email) {
-		super();
+				@Size(min = 3, max = 30) String password,
+				@Size(min = 3, max = 30) @Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!") String name,
+				@Size(min = 3, max = 30) @Pattern(regexp = "[a-zA-Z\\s]+$", message = "Check name!") String surname,
+				@Size(min = 4, max = 30) @Pattern(regexp = "[A-Za-z0-9+_.-]+@(.+)$", message = "Check name!") String email) {
 		this.username = username;
 		this.password = password;
 		this.name = name;
 		this.surname = surname;
 		this.email = email;
 	}
-	
-	
-	
-	
-	
 }
