eb10e0ae6aa9ac1f247240539b18fe0b1094c06c
Martinsglp <67226518+Martinsglp@users.noreply.github.com>
2020-07-16 11:59:12 +0300

    update
    created regUser controller, register function for usercontroller

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controllers;
 
 import com.example.demo.models.Flight;
+import com.example.demo.models.RegisteredUser;
 import com.example.demo.service.IAirportService;
 import com.example.demo.service.IFlightService;
+import com.example.demo.service.IRegisterService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,8 +27,32 @@ public class UserController {
 
     @Autowired
     IAirportService airportService;
-
-
+    
+    @Autowired
+    IRegisterService regService;
+/*
+    @GetMapping("/register")
+    public String getRegisterUser(RegisteredUser regUser) {
+    	return "insert-one-user-page";
+    }
+    
+    @PostMapping("/register")
+    public String postRegisterUser(@Valid RegisteredUser regUser, BindingResult result) {
+    	System.out.println(regUser);
+    	
+    	if(result.hasErrors()) {
+    		return "insert-one-user-page";
+    	}
+    	
+    	regService.registerRegUser(regUser.getUsername(), regUser.getPassword(), regUser.getName(), regUser.getSurname(), regUser.getEmail(), regUser.getType());
+    	//TODO change from admin to user page..
+    	return "redirect:/admin/show-all-users-page";
+    	
+    }
+    */
+    
+    
+    
     @GetMapping("/saveTestingData")
     public String getSaveTestingData() {
         System.out.println("saveTestingData");
@@ -50,5 +77,11 @@ public class UserController {
         model.addAttribute("byFlightPrice", Comparator.comparing(Flight::getFlightPrice));
         return "show-flights-by-airports-page";
     }
+    
+    
+    
+    
+    
+    
     
 }
