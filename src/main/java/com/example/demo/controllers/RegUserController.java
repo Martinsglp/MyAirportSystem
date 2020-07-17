a201b929457423f4535d3091b28dfaf9be910ec7
Martinsglp <67226518+Martinsglp@users.noreply.github.com>
2020-07-17 10:32:28 +0300

    Update RegUserController.java

--- src/main/java/com/example/demo/controllers/RegUserController.java
+++ src/main/java/com/example/demo/controllers/RegUserController.java
@@ -1,10 +1,20 @@
 package com.example.demo.controllers;
 
+import java.util.Comparator;
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.models.Flight;
+import com.example.demo.service.IFlightService;
 import com.example.demo.service.IRegisterService;
 
 @Controller
@@ -14,16 +24,30 @@ public class RegUserController {
 	@Autowired
 	IRegisterService regService;
 	
+	@Autowired
+	IFlightService flightService;
+	
 	
 	@GetMapping("/test")
 	public String test() {
 		return "hello-page";
 	}
 	
-	@GetMapping("/tests")
-	public String tests() {
-		return "hello-page";
-	}
 	
 	
+	
+	
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
